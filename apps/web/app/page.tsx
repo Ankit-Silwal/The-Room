542eebfdd5956b10f657432c8ee906669b0441f6
GubbinLubbin <silwalankit27@gmail.com>
2026-02-19 00:43:04 +0530

    Added the web sockets

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div >
+      <html>
+        <body>
+          <h1>This is a working website probably</h1>
+        </body>
+      </html>
+    </div>
+  );
+}
